270a4a28b656678ee5f00738a7f361a2a7e4d2ac
Clara Marinho <119897667+mclaramarinho@users.noreply.github.com>
2023-11-30 14:02:18 -0300

    Add files via upload

---
+++ api/index.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home:
+	return jsonify({"message": "You need to choose an endpoint. \nEndpoints available:\n- /substAtivas\n- /excipientes"}), 200
+	
+@app.route('/substAtivas')
+def substAtivas:
+	return jsonify({"message": "substAtivas endpoint"}), 200
+
+@app.route('/excipientes')
+def excipientes:
+	return jsonify({"message": "excipientes endpoint"}), 200
